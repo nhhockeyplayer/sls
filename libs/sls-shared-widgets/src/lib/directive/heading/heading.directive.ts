04795adb0a3c9be4d4319b9159b0c99c17a5d3d7
nhhockeylayer <meanstack@hotmail.com>
2022-05-23 10:50:36 -0400

    aligned heading directive using style binding

--- libs/sls-shared-widgets/src/lib/directive/heading/heading.directive.ts
+++ libs/sls-shared-widgets/src/lib/directive/heading/heading.directive.ts
@@ -1,29 +1,48 @@
 import {Directive, HostBinding, Input} from '@angular/core'
-import {THeadingType} from './heading'
+import {THeadingAlign, THeadingType} from './heading'
+import {DomSanitizer, SafeStyle} from '@angular/platform-browser'
 
 @Directive({
-  selector: 'h1[slsHeading], h2[slsHeading], h3[slsHeading], h4[slsHeading], h5[slsHeading], h6[slsHeading], div[slsHeading], span[slsHeading]'
+    selector: 'h1[slsHeading], h2[slsHeading], h3[slsHeading], h4[slsHeading], h5[slsHeading], h6[slsHeading], div[slsHeading], span[slsHeading]',
+    host: {
+        class: HeadingDirective.defaultClassName
+    }
 })
 export class HeadingDirective {
-  static defaultClassName = 'slsHeading'
-  /** Add optional ID to host
-   */
-  @HostBinding('attr.id')  @Input() id?: string
-  /** Add optional ID to host
-   */
-  @Input() type?: THeadingType = 'h2'
+    static defaultClassName = 'slsHeading'
+    /** Add optional ID to host
+     */
+    @HostBinding('attr.id') @Input() id?: string
+    /** Add optional type
+     *  default: h2
+     */
+    @Input() type?: THeadingType = 'h2'
 
-  @HostBinding('attr.role') heading: string = 'heading'
+    /** Add optional align
+     *  default: center
+     */
+    @Input() align: THeadingAlign = 'center'
 
-  /** Add extra class to host
-   */
-  @Input() className?: string
+    @HostBinding('attr.role') heading: string = 'heading'
 
-  @HostBinding('class') get classBinding() {
-    const classBinding = [
-      HeadingDirective.defaultClassName,
-      this.className
-    ].filter(Boolean).join(' ')
-    return classBinding
-  }
+    /** Add extra class to host
+     */
+    @HostBinding('attr.className') @Input() className?: string
+
+    @HostBinding('class') get classBinding() {
+        const classBinding = [
+            HeadingDirective.defaultClassName,
+            this.className
+        ]
+            .filter(Boolean)
+            .join(' ')
+        return classBinding
+    }
+
+    @HostBinding('style')
+    get myStyle(): SafeStyle {
+        return this.sanitizer.bypassSecurityTrustStyle(`text-align: ${this.align}`);
+    }
+
+    constructor(private sanitizer: DomSanitizer) {}
 }
--- libs/sls-shared-widgets/src/lib/directive/heading/heading.ts
+++ libs/sls-shared-widgets/src/lib/directive/heading/heading.ts
@@ -1,4 +1,3 @@
-
 export type THeadingType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'span' | 'div'
 
-export type THeadingAlign = 'left' | 'right' | 'center'
+export type THeadingAlign = 'justify' | 'center' | 'left' | 'right'
